79573c1cc0bcf2de7e8930e7699e8a34bedb130b
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:30:44 +0300

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworks
+{
+    internal class Program8
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+            string message;
+
+            while(isActive)
+            {
+                Console.WriteLine("Введите сообщение: ");
+                message = Console.ReadLine();
+                if(message == "exit")
+                {
+                    Console.WriteLine("Цикл завершен! ");
+                    isActive = false;
+                }
+            }
+        }
+    }
+}
