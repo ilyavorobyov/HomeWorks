04ec11cc04fa0c73ab9c3f4b193eb258941f4211
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:54:47 +0300

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program10
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(); 
+            int number = rand.Next(0,101);
+            int sum = 0;
+            int multipleNumber1 = 3;
+            int multipleNumber2 = 5;
+
+            for(int i =0; i < number; i++)
+            {
+                if(i % multipleNumber1 == 0 || i % multipleNumber2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Сумма всех положительный чисел меньше числа " + number + " которые кратны " + multipleNumber1 + " и " + multipleNumber2 + " равна: " + sum);
+
+
+           
+        }
+    }
+}
