9d3935c6ff6892d323b6db893701f301f86c12ae
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 13:50:15 +0300

    Add files via upload

---
+++ Program8v2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program8
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+            string message;
+            string stopLoop = "exit";
+
+            while (isActive)
+            {
+                Console.WriteLine("Введите сообщение: ");
+                message = Console.ReadLine();
+
+                if (message == stopLoop)
+                {
+                    Console.WriteLine("Цикл завершен! ");
+                    isActive = false;
+                }
+            }
+        }
+    }
+}
