78a4eb35d43af5e3cbe8b56d3970c20db9929d0b
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:10:51 +0300

    Add files via upload

---
+++ Program5v2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program6
+    {
+        static void Main(string[] args)
+        {
+            int userGold;
+            int crystalPrice = 5;
+            int countOfCrystalsToBuy;
+
+            Console.Write("Добро пожаловать в магазин! Сколько у вас золота?: ");
+            userGold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Цена одного кристалла: " + crystalPrice + " Сколько кристаллов вы хотите купить?: ");
+            countOfCrystalsToBuy = Convert.ToInt32(Console.ReadLine());
+            userGold -= crystalPrice * countOfCrystalsToBuy;
+            Console.Write("Золота осталось: " + userGold + " Число кристаллов: " + countOfCrystalsToBuy);
+            Console.ReadKey();
+        }
+    }
+}
