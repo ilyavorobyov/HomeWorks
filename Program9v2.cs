71dbfcc6b26f847830cc91382aed4fea6a4158f4
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 13:45:16 +0300

    Add files via upload

---
+++ Program9v2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program10
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int step = 7;
+            int maxValue = 96;
+
+            for (int i = 0; i < maxValue; i += step)
+            {
+                Console.WriteLine(number);
+                number += step;
+            }
+        }
+    }
+}
