9e96391ade0978939aba10929aea4f5d5848dca2
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 19:13:07 +0300

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MinNumber = 0;
+            const int MaxNumber = 100;
+
+            Random random = new Random();
+            int degree = 2;
+            int exponentiableNumber = 2;
+            int number = random.Next(MinNumber, MaxNumber);
+            int finalNumber;
+
+            while (Math.Pow(exponentiableNumber, degree) <= number)
+            {
+                degree++;
+            }
+            finalNumber = Convert.ToInt32(Math.Pow(exponentiableNumber, degree));
+            Console.WriteLine("Случайное число - " + number);
+            Console.WriteLine("Нужная степень двойки - " + degree);
+            Console.WriteLine("число два в найденной степени - " + finalNumber);
+        }
+    }
+}
