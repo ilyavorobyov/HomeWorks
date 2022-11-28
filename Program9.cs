954b1f4e1e66679bd4e33003b14701d56c3b82bb
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:41:11 +0300

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program9
+    {
+        static void Main(string[] args)
+        {
+            int countRepeats = 14;
+            int number = 5;
+
+            for(int i =0; i< countRepeats; i++)
+            {
+                Console.WriteLine(number);
+                number += 7;
+            }
+        }
+    }
+}
