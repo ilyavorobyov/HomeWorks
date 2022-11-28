7ed0263896c4a38cc822c61c20a4853030a612d0
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:20:56 +0300

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworks
+{
+    internal class Program7
+    {
+        static void Main(string[] args)
+        {
+            int repeatsCount;
+            string message;
+            Console.Write("Введите сообщение для повтора: ");
+            message = Console.ReadLine();
+            Console.Write("Введите, сколько раз нужно повторить вывод сообщения: ");
+            repeatsCount = Convert.ToInt32(Console.ReadLine());
+
+            for(int i =0; i< repeatsCount; i++) 
+            { 
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
