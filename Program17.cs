eee36ce4c8875429f66b36a88611f4909c128e94
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 19:58:36 +0300

    Add files via upload

---
+++ Program17.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Parenthesis = "(()(())))(()))";
+            const char LeftParenth = ')';
+
+            int countOfRightParenthesis = 0;
+            int countOfLeftParenthesis = 0;
+            int parenthesisNesting = 0;
+            int firstCounter = 0;
+            int secondCounter = 0;
+
+            for (int i = 0; i < Parenthesis.Length; i++)
+            {
+                if (Parenthesis[i] == LeftParenth)
+                {
+                    countOfLeftParenthesis++;
+                    firstCounter++;
+                }
+                else
+                {
+                    countOfRightParenthesis++;
+                    firstCounter = 0;
+                }
+
+                if (firstCounter > 0)
+                {
+                    if (secondCounter < firstCounter)
+                    {
+                        secondCounter = firstCounter;
+                        parenthesisNesting = secondCounter;
+                    }
+                }
+            }
+
+            if (countOfRightParenthesis == countOfLeftParenthesis)
+            {
+                Console.WriteLine("скобки закрытые");
+            }
+            else
+            {
+                Console.WriteLine("скобки не закрытые");
+            }
+
+            Console.WriteLine("Максимальная вложенность: " + parenthesisNesting);
+        }
+    }
+}
