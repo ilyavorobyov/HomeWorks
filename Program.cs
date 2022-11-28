86235e76a43af2b8cd52e83a56a6207887f352f3
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 09:40:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHealth;
+            bool isFlying;
+            string userSurname;
+            byte smallNumber;
+            float enemyDamage;
+            char locationType;
+            uint maxScore;
+            long countryBudget;
+            ushort moveSpeed;
+            double minValue;
+        }
+    }
+}
+
+
+
