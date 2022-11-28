e417630eed896872e8430829c0b01e0b58a9da22
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 10:18:57 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            string userName = " Ivanov";
+            string userSurname = " Stepan";
+            Console.WriteLine("До перестановки ИМЯ и ФАМИЛИЯ:" + userName + userSurname);
+            (userName, userSurname) = (userSurname, userName);
+            Console.WriteLine("После перестановки ИМЯ и ФАМИЛИЯ:" + userName + userSurname);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
