fe4598911067ca5d728926c18e35fb1a390228ba
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 10:40:44 +0300

    Add files via upload

---
+++ Program3v2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int rowCapacity = 3;
+
+            int numberOfCompleteRows = pictures / rowCapacity;
+            int picturesWithoutRow = pictures % rowCapacity;
+
+            Console.WriteLine("Полностью заполненных рядов картинок: "  + numberOfCompleteRows + " , а картинок без ряда осталось: " + picturesWithoutRow);
+        }
+    }
+}
