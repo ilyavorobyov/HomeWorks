f9cf2b64bf1017f3970a4acea981c4f8209344cb
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-28 11:05:01 +0300

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program6
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int timeOfReceipt = 10;
+            int hourDuration = 60;
+            int totalWaitingTime;
+            int waitingHours;
+            int waitingminutes;
+
+            Console.Write("Сколько людей в очереди?: ");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+            totalWaitingTime = peopleInLine * timeOfReceipt;
+            waitingHours = totalWaitingTime / hourDuration;
+            waitingminutes = totalWaitingTime % hourDuration;
+            Console.WriteLine("Вам осталось ждать: " + waitingHours + " часов, и: " + waitingminutes + " минут");
+            Console.ReadKey();
+        }
+    }
+}
