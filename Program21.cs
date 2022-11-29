0223d07b25539b39f55fb9dd1689a4f43eb9e3c0
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-11-29 18:05:03 +0300

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "You win some. You lose some.";
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
