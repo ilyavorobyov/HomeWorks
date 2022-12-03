0eea0ff836e05f0e09be6c2f44bbbb1548b58989
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-03 20:51:35 +0300

    Add files via upload

---
+++ Program13v5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            int rowLenght;
+            string middleRow;
+            string outsideRow;
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            middleRow = symbol + name + symbol;
+            rowLenght = middleRow.Length;
+            outsideRow = new string(symbol, rowLenght);
+
+            Console.WriteLine(outsideRow);
+            Console.WriteLine(middleRow);
+            Console.WriteLine(outsideRow);
+            Console.ReadKey();
+        }
+    }
+}
