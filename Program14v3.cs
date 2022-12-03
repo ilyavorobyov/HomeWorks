7dc7564291eeaa88febc4d91db6b7a10cbf0ede8
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-03 21:21:45 +0300

    Add files via upload

---
+++ Program14v3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "Qwerty1";
+            string secretMessage = "Ты ввел пароль правильно! Молодец!";
+            string enteredPassword;
+            int inputCounter = 3;
+
+            for (int i = 0; i <= inputCounter;)
+            {
+                Console.Write($"Введите пароль (осталось {inputCounter} попыток): ");
+                enteredPassword = Console.ReadLine();
+
+                if (enteredPassword == password)
+                {
+                    Console.WriteLine("Секретное послание:");
+                    Console.WriteLine(secretMessage);
+                    break;
+                }
+                else
+                {
+                    inputCounter--;
+                    if (inputCounter <= 0)
+                    {
+                        Console.WriteLine("Попытки кончились! Попробуй позже");
+                        break;
+                    }
+                    Console.WriteLine("Пароль неправильный, осталось " + inputCounter + " попыток");
+                }
+            }
+        }
+    }
+}
