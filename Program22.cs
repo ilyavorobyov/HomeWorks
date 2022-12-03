9d0676e05e829fdb99c1d44b77e5dc4355603c16
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-03 22:06:52 +0300

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTryParse = true;
+            int number;
+            TypeNumber(ref isTryParse, out number);
+        }
+
+        static void TypeNumber(ref bool isTryParse, out int number)
+        {
+            number = 0;
+
+            while (isTryParse)
+            {
+                Console.Write("Введите число: ");
+                string userInput = Console.ReadLine();
+                bool isNumber = int.TryParse(userInput, out number);
+
+                if (isNumber == true)
+                {
+                    Console.WriteLine($"Преобразование прошло успешно. Число: {number}");
+                    isTryParse = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Преобразование завершилось неудачно");
+                }
+            }
+        }
+    }
+}
