b3d86b0ee66ed2ba0dc90d53c6d57f4aaa54d29a
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-06 18:15:47 +0300

    Add files via upload

---
+++ Program17v3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string parenthesis = "(())";
+            char rightParenth = '(';
+            char leftParenth = ')';
+            int depth = 0;
+            int maxDepth = 0;
+
+            foreach (char symbol in parenthesis)
+            {
+                if (symbol == rightParenth)
+                {
+                    depth++;
+                }
+                else if (symbol == leftParenth)
+                {
+                    depth--;
+                }
+
+                if (depth < 0)
+                {
+                    break;
+                }
+
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                }
+            }
+
+            if (depth == 0)
+            {
+                Console.WriteLine("Корректное скобочное выражение. Глубина: " + maxDepth);
+            }
+            else
+            {
+                Console.WriteLine("Некорректное скобочное выражение");
+            }
+        }
+    }
+}
