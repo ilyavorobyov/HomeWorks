9ba0df67c5090118c7f4e6cd9b9f5ad20e81185a
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-10 14:33:36 +0300

    Add files via upload

---
+++ Program16v2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 0;
+            int maxNumber = 100;
+            int degree = 2;
+            int exponentiableNumber = 2;
+            int numberForExponentiation = 2;
+            Random random = new Random();
+            int number = random.Next(minNumber, maxNumber);
+
+            while ((exponentiableNumber *= numberForExponentiation) <= number)
+            {
+                degree++;
+            }
+
+            Console.WriteLine("Случайное число " + number);
+            Console.WriteLine("Нужная степень двойки " + degree);
+            Console.WriteLine("Число в найденной степени " + exponentiableNumber);
+        }
+    }
+}
