7c5f6530a533fe8957e0fb349301212747c1b530
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2022-12-11 10:50:53 +0300

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shuffle();
+        }
+
+        static void Shuffle()
+        {
+            int[] massiveToShuffle = { 3, 12, 35, 22, 119, 62, 11, 4, 85, 12 };
+            Random random = new Random();
+
+            for (int i = massiveToShuffle.Length - 1; i >= 1; i--)
+            {
+                int randomIndex = random.Next(i + 1);
+                int shuffleIndex = massiveToShuffle[randomIndex];
+                massiveToShuffle[randomIndex] = massiveToShuffle[i];
+                massiveToShuffle[i] = shuffleIndex;
+                Console.WriteLine(massiveToShuffle[i]);
+            }
+        }
+    }
+}
