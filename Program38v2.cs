8ff276b9d42447d1e7c6f07efa40bf1d841d6ac7
ilyavorobyov <30550120+ilyavorobyov@users.noreply.github.com>
2023-01-07 13:04:52 +0300

    Add files via upload

---
+++ Program38v2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(10, 9);
+            PlayerShow playerShow = new PlayerShow('$');
+            playerShow.DrawSymbol(player._yPosition, player._xPosition);
+        }
+    }
+
+    class Player
+    {
+        public int _xPosition { get; private set; }
+        public int _yPosition { get; private set; }
+
+        public Player(int xPosition, int yPosition)
+        {
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+        }
+    }
+
+    class PlayerShow
+    {
+        private char _symbol;
+
+        public PlayerShow(char symbol)
+        {
+            _symbol = symbol;
+        }
+
+        public void DrawSymbol(int _xPosition, int _yPosition)
+        {
+            Console.SetCursorPosition(_yPosition, _xPosition);
+            Console.Write(_symbol);
+        }
+    }
+}
