c137cdb943fdc0da5065b7c540f4435d00163fe8
Илья <30550120+ilyavorobyov@users.noreply.github.com>
2023-11-27 17:43:38 +0300

    Add files via upload

---
+++ CorrectName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp71
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int minValue = 1;
+            int maxValue = 3;
+            Console.WriteLine(ClampNumber(number, minValue, maxValue));
+        }
+
+        public static int ClampNumber(int number, int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException();
+
+            if (number < minValue)
+                return minValue;
+            else if (number > maxValue)
+                return maxValue;
+            else
+                return number;
+        }
+    }
+}
