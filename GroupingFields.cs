f9e9accb2d6ec0ae349324b7d7c8c663e6a9b2ba
Илья <30550120+ilyavorobyov@users.noreply.github.com>
2023-12-03 10:54:07 +0300

    Add files via upload

---
+++ GroupingFields.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GroupingFields
+{
+    public class Player
+    {
+        private string _name;
+        private int _age;
+        private float _speed;
+        private float _movementDirectionX;
+        private float _movementDirectionY;
+
+        private void Move()
+        {
+            //Do move
+        }
+    }
+
+    public class Weapon
+    {
+        private int _damage;
+        private float _rechargeTime;
+
+        private void Attack()
+        {
+            //attack
+        }
+
+        private bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
